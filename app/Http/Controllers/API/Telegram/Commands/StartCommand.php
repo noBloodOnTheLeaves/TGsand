c4dceec5937717e10dd95acb65a73a593f3930a6
Igor <igor-skromnik@yandex.ru>
2023-03-12 13:50:44 +0300

    update-start-command-and-refactoring

--- app/Http/Controllers/API/Telegram/Commands/StartCommand.php
+++ app/Http/Controllers/API/Telegram/Commands/StartCommand.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\API\Telegram\Commands;
 
+use Faker\Core\File;
+use Illuminate\Support\Facades\Storage;
 use Telegram\Bot\Actions;
+use Telegram\Bot\Api;
 use Telegram\Bot\Commands\Command;
+use Telegram\Bot\FileUpload\InputFile;
 
 class StartCommand extends Command
 {
@@ -11,12 +15,11 @@ class StartCommand extends Command
     /**
      * @var string Command Name
      */
-    protected $name = "start";
-
+    protected $name = "вопрос";
     /**
      * @var string Command Description
      */
-    protected $description = "/start - для начала взаимодействия с ботом";
+    protected $description = "Задай вопрос, а я на него отвечу.";
 
     /**
      * @inheritdoc
@@ -27,29 +30,81 @@ class StartCommand extends Command
         // the user/chat id who triggered this command.
         // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
         // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
-        $this->replyWithMessage(['text' => 'Ну че епта, вот и понеслась']);
+        $token = env('TELEGRAM_BOT_TOKEN');
+        $tg = new Api($token);
 
-        // This will update the chat status to typing...
-        $this->replyWithChatAction(['action' => Actions::TYPING]);
+        //Получить объект сообщения
+        $updates = $this->getUpdate();
+        $question = str_replace('\/вопрос','', $updates->message->text);
 
-        // This will prepare a list of available commands and send the user.
-        // First, Get an array of all registered commands
-        // They'll be in 'command-name' => 'Command Handler Class' format.
-        $commands = $this->getTelegram()->getCommands();
+        $this->sendAction($tg, $updates);
+        $thinking = $this->sendThinkSticker($tg, $updates);
 
-        // Build the list
-        $response = '';
-        foreach ($commands as $name => $command) {
-            $response .= sprintf('/%s - %s' . PHP_EOL, $name, $command->getDescription());
-        }
+        $this->sendAction($tg, $updates);
+        sleep(4);
+        $this->deleteMessage($thinking, $tg);
 
-        // Reply with the commands list
-        $this->replyWithMessage(['text' => $response]);
+
+        /*
+         * 1) Сценарий команда вопрос после которой идет сам вопрос
+         * 2) Получить кекст вопроса и id чата куда отправлять
+         * 3) Ответить думающим стикером и получить Id думающего стикера, чтобы заменить его на ответ потом
+         * 4) Как придет ответ заменить стикер
+        */
 
         // Trigger another command dynamically from within this command
         // When you want to chain multiple commands within one or process the request further.
         // The method supports second parameter arguments which you can optionally pass, By default
         // it'll pass the same arguments that are received for this command originally.
-        $this->triggerCommand('subscribe');
+        //$this->triggerCommand('subscribe');
+    }
+
+    private function getThinkingPath(Api $tg): ?string
+    {
+        $pathFile =$tg->getFile([
+            'file_id' => 'AAMCAgADGQEAAgZ6ZAzG7hAx0AeCIMWIcDc3WNzQR7cAAl8AA9vbfgABhLBUmxGuyuMBAAdtAAMvBA'
+        ])->file_path;
+        $token = env('TELEGRAM_BOT_TOKEN');
+        return "https://api.telegram.org/file/bot$token/$pathFile";
+    }
+
+    private function sendThinkSticker($tg, $updates)
+    {
+        $stickerParams = [
+            'chat_id' => $updates->message->chat->id,
+            'sticker' => InputFile::create($this->getThinkingPath($tg)),
+        ];
+
+        if($updates->message->is_topic_message){
+            $stickerParams['message_thread_id'] = $updates->message->message_thread_id;
+        }
+
+        return $tg->sendSticker($stickerParams);
+    }
+
+    private function sendAction($tg, $updates, $type = 'typing')
+    {
+        $actionParams = [
+            'action' => $type,
+            'chat_id' => $updates->message->chat->id,
+        ];
+        if ($updates->message->is_topic_message) {
+            $actionParams['message_thread_id'] = $updates->message->message_thread_id;
+        }
+        $tg->sendChatAction($actionParams);
+    }
+
+    private function deleteMessage($thinking, $tg)
+    {
+        $answerParams = [
+            'chat_id' => $thinking->chat->id,
+            'message_id' => $thinking->messageId,
+        ];
+
+        if($thinking->is_topic_message){
+            $answerParams['message_thread_id'] = $thinking->message_thread_id;
+        }
+
+        $tg->deleteMessage($answerParams);
     }
 }
--- app/Http/Controllers/API/Telegram/WebHook/TelegramWebHookController.php
+++ app/Http/Controllers/API/Telegram/WebHook/TelegramWebHookController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\API\Telegram\WebHook;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramWebHookController extends Controller
 {
     public function index(Request $request): void
     {
+        Telegram::commandsHandler(true);
         Log::debug($request->all());
     }
 }
